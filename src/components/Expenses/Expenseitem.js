2625e463b26b63b50287c6a9d034332a65438c40
manikandan <baskarmanikandan48@gmail.com>
2022-08-26 22:08:57 +0530

    Second commit

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -16,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button>Click Me!</button>
     </Card>
   );
 };
